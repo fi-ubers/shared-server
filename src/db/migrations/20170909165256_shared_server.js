d91f476bf1655c7dbd22c5b1e6cd902b68c33cf6
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-09-17 15:43:14 -0300

    Merging

--- src/db/migrations/20170909165256_shared_server.js
+++ src/db/migrations/20170909165256_shared_server.js
@@ -3,12 +3,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('app_servers', function(table) {
   		table.increments('id').primary();
-  		//table.string('id').notNullable().unique();
   		table.string('_ref');
-  		table.string('createdBy').notNullable();
+  		table.string('createdBy');
   		table.timestamp('createdTime');
-  		table.string('name').notNullable();
-  		table.timestamp('lastConnection').notNullable();
+  		table.string('name');
+  		table.timestamp('lastConnection');
   	});
 };
 
