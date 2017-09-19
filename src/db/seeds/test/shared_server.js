fc4d560d9131c2e337ed5c8a684adc0344c16412
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-09-19 00:32:31 -0300

    Fixing tests

--- src/db/seeds/test/shared_server.js
+++ src/db/seeds/test/shared_server.js
@@ -7,16 +7,16 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('app_servers').insert({
         createdBy: 4, 
-        createdTime: '2017-09-18 15:30:23',
+        createdTime: '2017-09-18T18:30:23.000Z',
         name: 'Fiuber',
-        lastConnection: '2017-09-18 15:30:23'
+        lastConnection: '2017-09-18T18:30:23.000Z'
       });
     }).then(function() {
         return knex('app_servers').insert({
           createdBy: 2, 
-          createdTime: '2017-09-15 10:40:14',
+          createdTime: '2017-09-15T10:40:14.000Z',
           name: 'AppJ',
-          lastConnection: '2017-09-15 10:40:14'
+          lastConnection: '2017-09-15T10:40:14.000Z'
       });
     });
 };
