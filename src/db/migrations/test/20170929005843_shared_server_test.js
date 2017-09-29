fe1fcd7f25b04178b9acf74a69036a2bdcefac3c
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-09-29 01:33:56 -0300

    Token only from queries

---
+++ src/db/migrations/test/20170929005843_shared_server_test.js
@@ -0,0 +1,60 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+    
+	  knex.schema.createTableIfNotExists('app_servers', function(table) {
+	  		table.increments('id').primary();
+	  		table.string('_ref');
+	  		table.string('createdBy');
+	  		table.timestamp('createdTime').defaultTo(knex.fn.now());
+	  		table.string('name');
+	  		table.timestamp('lastConnection');
+	  }),
+	  
+	  knex.schema.createTableIfNotExists('business_users', function(table) {
+	  		table.increments('id').primary();
+	  		table.string('_ref');
+	  		table.string('username');
+	  		table.string('password');
+	  		table.string('name');
+	  		table.string('surname');
+	  		table.specificType('roles', 'text[]');
+	  }),
+	  
+	  knex.schema.createTableIfNotExists('application_users', function(table) {
+	  		table.increments('id').primary();
+	  		table.string('_ref');
+	  		table.string('applicationOwner');
+	  		table.string('type');
+	  		table.specificType('cars', 'json[]');
+	  		table.string('username');
+	  		table.string('name');
+	  		table.string('surname');
+	  		table.string('country');
+	  		table.string('email');
+	  		table.string('birthdate');
+	  		table.specificType('images', 'text[]');
+	  		table.specificType('balance', 'json[]');
+	  }),
+	  
+	  knex.schema.createTableIfNotExists('blacklist', function(table) {
+	  		table.string('jti').primary();
+	  }),
+	  
+	  knex.schema.createTableIfNotExists('app_tokens', function(table) {
+	  		table.integer('id').primary();
+	  		table.string('token');
+	  })
+	  
+    ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfExists('app_servers'),
+        knex.schema.dropTableIfExists('business_users'),
+        knex.schema.dropTableIfExists('application_users'),
+        knex.schema.dropTableIfExists('blacklist'),
+        knex.schema.dropTableIfExists('app_tokens')
+    ])
+};
--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -2,10 +2,8 @@ var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-function fromHeaderOrQuerystring(req) {
-    	if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-    		return req.headers.authorization.split(' ')[1];
-    	} else if (req.query && req.query.token) {
+function fromQuerystring(req) {
+    	if (req.query && req.query.token) {
       		return req.query.token;
 	}
 	return null;
@@ -14,12 +12,12 @@ function fromHeaderOrQuerystring(req) {
 module.exports = {
 	appVerify: expressJwt({ 
 		secret: process.env.APP_KEY,
-		getToken: fromHeaderOrQuerystring
+		getToken: fromQuerystring
 	}),
 	
 	businessVerify: expressJwt({ 
 		secret: process.env.BUSINESS_USER_KEY,
-		getToken: fromHeaderOrQuerystring
+		getToken: fromQuerystring
 	}),
 	
 	checkExpirationError: function (err, req, res, next) {
