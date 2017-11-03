fe2723379d8c2c405d07b83e9baf6fb4dd4ece95
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-11-03 20:44:07 -0300

    Include payment API config

--- src/controllers/paymethodsController.js
+++ src/controllers/paymethodsController.js
@@ -1,10 +1,12 @@
-var logger = require('./../logger');
+const logger = require('./../logger');
+const paymentAPI = require('../../config/paymentAPI');
 
 /** @module paymethodsController */
 module.exports = {
 
 	/** Lists all the payment methods supported by the server. */
 	getPaymethods : function(req, res) {
+		var uri = "/paymethods";
 		logger.info("GET at /paymethods");
 		res.send("Payment methods supported by the server");
 	}
