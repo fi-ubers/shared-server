6f8c711aed5514ca3d31737e31ad374776ad8666
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-11-16 01:55:14 -0300

    Include user roles info in local storage

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { tokenNotExpired } from 'angular2-jwt';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
   authToken: any;
   user: any;
+  jwtHelper: JwtHelper = new JwtHelper();
   
   constructor(private http:Http) { }
   
   authenticateUser(user) {
-    let uri = 'api/token';
+    //let uri = 'api/token';
+    let uri = 'http://localhost:5000/api/token';
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.post(uri, user, {headers: headers})
@@ -28,7 +30,8 @@ export class AuthService {
   }
   
   getProfile() {
-    let uri = 'api/business-users/me';
+    //let uri = 'api/business-users/me';
+    let uri = 'http://localhost:5000/api/business-users/me';
     let headers = new Headers();
     this.loadToken();
     headers.append('Content-Type', 'application/json');
@@ -39,6 +42,7 @@ export class AuthService {
   storeUserData(token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('user_roles', this.jwtHelper.decodeToken(token).roles);
     this.authToken = token;
     this.user = user;
   }
